01df8553283673e92ff7c81536f7670759a4ebd7
Jacob <jkolker@hawk.iit.edu>
2019-03-03 21:51:21 -0600

    linted code

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -1,6 +1,5 @@
 (function(){
   // Check if browser is new enough
-  console.log("good");
   if (!('querySelector' in document && 'addEventListener' in document)) {
     return;
   }
@@ -10,7 +9,7 @@
   // functions to remove characters not used for input
   function clean_nonnumbers(value) {
     // returns string with no characters that aren't numbers
-    return value.replace(/\D/g,'');
+    return (value.replace(/\D/g,''));
   }
 
   function strip_us_country_code(value) {
@@ -18,53 +17,37 @@
     return value.replace(/^1/,'');
   }
 
-  function clean_whitespace(value) {
-    // returns value without any spaces
-    return value.replace(/\s/g, '');
-  }
-
-
-
-    // Phone validity functions
+    // Phone validity function
   function validate_us_phone(value) {
     value=clean_nonnumbers(value);
-    if (value.length==11)
-    {
+    if (value.length==11){
     var value = strip_us_country_code(clean_nonnumbers(value));
     }
     console.log(value);
-    if (value.length==10)
-    {
+    if (value.length==10){
 
       return(true);
-    }
-    else {
+    }else {
       return(false);
     }
   }
-
+  //  Finds if value is not empty
   function not_empty(value) {
 
-    if(value == "")
-    {
+    if(value == ""){
       return(false);
-    }
-    else
-    {
+    }else{
       return(true);
     }
   }
-
-  function validate_email(value)
-  {
+  // Finds if email is valid
+  function validate_email(value){
     var re = /^[^@\s]+@[^@\s]+$/g;
     console.log(value);
     return(re.test(value));
 
   }
-/*
 
-*/
   document.addEventListener('DOMContentLoaded',function(event){
     //  Find the form inputs
     var quote_form = document.querySelector('#form');
@@ -74,73 +57,52 @@
     var company = document.querySelector('#company');
     var info = document.querySelector('#info');
     var submit = document.querySelector('#submit');
-    console.log("loaded");
 
+    //  disable submit button
     submit.disabled=true;
 
+    //  Check the values of functions and see if the meet requirements
     quote_form.addEventListener('keyup',function(){
-      console.log(validate_us_phone(phone.value));
-      console.log(not_empty(name.value));
-      console.log(validate_email(email.value));
 
-      if(validate_email(email.value))
-      {
+      if(validate_email(email.value)){
         email.className='valid';
-      }
-      else
-      {
+      }else{
         email.className='invalid';
       }
 
-      if(not_empty(name.value))
-      {
+      if(not_empty(name.value)){
         name.className='valid';
-      }
-      else
-      {
+      }else{
         name.className='invalid';
       }
 
-      if(not_empty(company.value))
-      {
+      if(not_empty(company.value)){
         company.className='valid';
-      }
-      else
-      {
+      }else{
         company.className='invalid';
       }
 
-      if(not_empty(info.value))
-      {
+      if(not_empty(info.value)){
         info.className='valid';
-      }
-      else
-      {
+      }else{
         info.className='invalid';
       }
 
-      if(validate_us_phone(phone.value))
-      {
+      if(validate_us_phone(phone.value)){
         phone.className='valid';
-      }
-      else
-      {
+      }else{
         phone.className='invalid';
       }
-      if(validate_email(email.value)==true && not_empty(name.value)==true &&not_empty(company.value)==true && not_empty(info.value)==true && validate_us_phone(phone.value)==true)
-      {
+
+      //  unlock the submit button if all values are valid
+      if(validate_email(email.value)==true && not_empty(name.value)==true &&not_empty(company.value)==true && not_empty(info.value)==true && validate_us_phone(phone.value)==true){
         submit.className='valid';
         submit.disabled=false;
-      }
-      else
-      {
-          submit.className='invalid';
-          submit.disabled=true;
+      }else{
+        submit.className='invalid';
+        submit.disabled=true;
       }
 
     });
-    //console.log(validate_us_phone(phone));
-
-
 
   });
